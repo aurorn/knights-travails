4a407745795a45a584948d5cfa93ff51fb27a887
Aurorn <aurornispro@gmail.com>
2024-07-03 14:02:34 -0400

    Updated imports as board and knight_m is called in the buttons file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import { knightMoves } from "./modules/knight_m";
-import { createChessBoard } from "./modules/board";
 import './styles.css';
+import { createButtons } from "./modules/buttons";
 
 const appMain = document.getElementById('app-main');
-appMain.appendChild(createChessBoard());
-
-// console.log tests
-knightMoves([0, 0], [3, 3]);
-knightMoves([3, 3], [0, 0]);
-knightMoves([0, 0], [7, 7]);
+createButtons(appMain);
